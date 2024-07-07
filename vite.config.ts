d83b5b110e954f59efb93549423e0dae0eeaa262
Ismail Habibov <ismailaga123@gmail.com>
2024-07-08 01:49:07 +0300

    todo

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/",
+  base:"/todo",
   test:{
 	globals:true,
 	environment: 'jsdom',
